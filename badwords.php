afba7de50e98aee5d77b7c4876277d0ac801be1a
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-16 17:46:15 +0100

    add censored text

--- badwords.php
+++ badwords.php
@@ -1,7 +1,12 @@
 <?php
 
 $text = $_GET['text'];
-$badword = $_GET['badword']
+$badword = $_GET['badword'];
+
+$text_length = strlen($text);
+
+$censored_text = str_replace($badword, '***', $text);
+$new_text_length = strlen($censored_text);
 
 ?>
 
@@ -17,17 +22,22 @@ $badword = $_GET['badword']
 </head>
 <body>
     <div class="container my-5">
-        <h1 class="text-warning">Testo non censurato</h1>
-        <p>
-            <?= $text ?>
-        </p>
+        <div id="text">
+            <h1 class="text-warning">Testo non censurato</h1>
+            <p><?= $text ?></p>
+            <span>Lunghezza del paragrafo: <?= $text_length ?></span>
+        </div>
         
-        <h1 class="text-success">Testo censurato</h1>
-        <p>Da ottenere</p>
-        <h3>Parola Censurata</h3>
-        <p>
-            <?= $badword ?>
-        </p>
+        <div id="text-censored">
+            <h1 class="text-success mt-5">Testo censurato</h1>
+            <p><?= $censored_text ?></p>
+            <span>Lunghezza del paragrafo: <?= $new_text_length ?></span>
+            
+            <h4 class="mt-5">Parola Censurata</h4>
+            <p>
+                <?= $badword ?>
+            </p>
+        </div>
     </div>
 </body>
 </html>
